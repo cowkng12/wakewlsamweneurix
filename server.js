473bbe95a7a397ce44c23b44328b57c8be7c729b
Пользователь <cowkng12@users.noreply.github.com>
2026-08-09 13:21:41 +0300

    Use Telegram menu button for app opening

--- server.js
+++ server.js
@@ -1579,14 +1579,6 @@ if (botToken) {
     ])
   }
 
-  function catalogReplyKeyboard(language, telegramId = '') {
-    const text = botText[language]
-
-    return Markup.keyboard([[Markup.button.webApp(text.shop, userWebAppUrl(telegramId))]])
-      .resize()
-      .oneTime(false)
-  }
-
   function subscriptionKeyboard(language) {
     const text = botText[language]
 
@@ -1719,7 +1711,6 @@ if (botToken) {
   async function sendMainMenu(context, language) {
     const name = context.from?.first_name || 'friend'
     await context.reply(botText[language].welcome(name), mainKeyboard(language, context.from?.id))
-    await context.reply(botText[language].shop, catalogReplyKeyboard(language, context.from?.id))
   }
 
   async function sendSubscriptionGate(context, language) {
@@ -2167,9 +2158,9 @@ if (botToken) {
   bot.launch()
     .then(async () => {
       await bot.telegram.setChatMenuButton({
-        menu_button: {
+        menuButton: {
           type: 'web_app',
-          text: 'Menu',
+          text: 'Open',
           web_app: { url: webAppUrl },
         },
       })
